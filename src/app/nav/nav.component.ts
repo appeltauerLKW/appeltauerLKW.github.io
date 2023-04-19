d9946bc69be04d494d04be07c040e103bee63e0a
appeltauerLKW <128358828+appeltauerLKW@users.noreply.github.com>
2023-04-19 10:34:15 +0200

    submit button and styles changed

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+    triggerEvent() {
+      let url = 'http://innoeva-pricing-broker-cc-cmp.test.lkw-walter.com/innoeva-pricing-broker-cc-cmp/api/reloadConfigs';
+      let body = {}
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic cHJpY2luZy1icm9rZXItdGVzdDppU2ZhREtWSGQ4eDhjWlkzaGlkdThtY1g='
+        }
+      })
+      .then((response) => response.json())
+      .then(json => {
+        // Handle the response here
+      }); 
+    }
 }
